4e8b596d3d3cc1ec3a251e471a873ad21fd88217
João Magalhães <joamag@gmail.com>
2023-02-02 17:22:35 +0000

    fix: types

--- types/base.d.ts
+++ types/base.d.ts
@@ -2,6 +2,5 @@ export interface API {}
 
 export declare class API implements API {
     constructor(kwargs?: object);
-    base(): Promise<unknown>;
     info(): Promise<object>;
 }
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,2 @@
 export * from "./base";
-export * from "./client";
-export * from "./person";
-export * from "./project";
-export * from "./tag";
+
